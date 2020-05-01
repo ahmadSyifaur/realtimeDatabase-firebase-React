6837258c9e28a7b4a84ad90ea49f98e7456f8a05
AhmadSyifaur <ahmadsyifaur11@gmail.com>
2020-05-01 22:49:42 +0700

    done

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBnhfJfFewbboIqUBWq4tlMtS16hlyK2AI",
+    authDomain: "react-firebase-modul-10.firebaseapp.com",
+    databaseURL: "https://react-firebase-modul-10.firebaseio.com",
+    projectId: "react-firebase-modul-10",
+    storageBucket: "react-firebase-modul-10.appspot.com",
+    messagingSenderId: "905258645038",
+    appId: "1:905258645038:web:66339736085385094091bf"
+  };
+
+  export default firebaseConfig;
